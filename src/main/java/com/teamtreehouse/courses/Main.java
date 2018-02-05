432150806908457d1390c2a720d1f7d4b5bedce0
Michael Chan <mikecchan0@gmail.com>
2018-02-05 10:25:18 -0500

    init

---
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -0,0 +1,31 @@
+package com.teamtreehouse.courses;
+
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+public class Main {
+    public static void main(String[] args) {
+        //get("/hello", (req, res) -> "Hello World");
+
+        get("/", (req, res) -> {
+            Map<String,String> model = new HashMap<>();
+            model.put("username", req.cookie("username"));
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/sign-in", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            String username = req.queryParams("username");
+            res.cookie("username", username);
+            model.put("username", username);
+            return new ModelAndView(model, "sign-in.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
